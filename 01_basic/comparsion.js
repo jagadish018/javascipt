fb221d857c0c97a65ea227f4b5170e9d045b0c2b
Jagadish C K <150452462+jagadish018@users.noreply.github.com>
2024-08-15 06:22:18 +0000

    container config done

---
+++ 01_basic/comparsion.js
@@ -0,0 +1,16 @@
+//Array ,Object and function
+//Array
+let arr=["mahesh","jagadish","mohan"]
+console.log(arr[2])
+
+//object
+
+let myObj={
+    name:"jagadish",
+    age:23
+}
+console.log(myObj.name)
+//*************************************************************
+//stack    (premitive )  , Heap (Non premitive   )
+//which create the          which points the heap memory
+//copies
---
+++ 01_basic/string.js
@@ -0,0 +1,12 @@
+const name=new String('Jagadish welcome to javascript');
+
+/*console.log(name.length);
+console.log(name.toUpperCase())
+console.log(name.charAt(3))
+console.log(name.indexOf('g'))*/
+
+console.log(name.charCodeAt(1))// which returns the ASCII value
+console.log(name.replace('Jagadish','Jagan'))
+console.log(name.includes('welcome'))
+name.substring(0,2)
+name.sustr(2,5)
