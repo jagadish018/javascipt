04afca4f09696a9efe32d35a79eb60e6c2f9969a
Jagadish C K <150452462+jagadish018@users.noreply.github.com>
2024-08-15 06:23:03 +0000

    container config done

--- 01_basic/conversion_operation.js
+++ 01_basic/conversion_operation.js
@@ -12,3 +12,20 @@ console.log(stoi)
 //"33abc"=>Nan
 //true=>1
 
+//**************************Operations******************************
+
+let value=3
+negvalue=-value;
+console.log(negvalue)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
---
+++ 01_basic/number.js
@@ -0,0 +1,21 @@
+/*const score=400
+console.log(score)
+const balance=new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(2))
+*/
+
+//********************** Math ******************** */
+let num=[1,5,8,2,4]
+/*
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.46))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.8))
+*/
+console.log(Math.random())
+
